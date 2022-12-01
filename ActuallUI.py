65a7145a7f8fb4f0a1a9ed6de0ed577b98d74df1
jonas <yojonas.solberg@gmail.com>
2022-12-01 09:32:30 +0100

    Test på UI

---
+++ ActuallUI.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import modul_TTS
+from tkinter import messagebox
+root = Tk()
+root.title("The Game")
+root.geometry("900x800")
+#BreddeXHøyde
+
+#tekst som sier hei
+header1 = Label(root, text="Welcome to the the game")
+header1.grid(row=1,column=2)
+
+#logo for spiller
+my_img = ImageTk.PhotoImage(Image.open("bilde2.png"))
+myLabel = Label(image=my_img)
+myLabel.grid(row=2, column=3)
+
+#knapp for å starte selve spillet
+startButton = Button(root, text="Start", width=25)
+startButton.grid(row=3, column=5)
+
+#knapp for å avslutte
+exitButton = Button(root, text="exit", command=root.quit, width=25)
+exitButton.grid(row=3, column=3, rowspan=3)
+
+
+
+
+root.mainloop()
---
+++ windowTest.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+root = Tk()
+root.title("tittel på side")
+
+vertical = Scale(root, from_=0, to=200)
+vertical.pack()
+
+horizontal = Scale(root, from_=0, to=200, orient=or)
+
+
+def open():
+    global my_img
+    top = Toplevel()
+    label = Label(top, text="Hello World").pack()
+    my_img = ImageTk.PhotoImage(Image.open("bilde2.png"))
+    label1 = Label(top, image=my_img).pack()
+    btn2 = Button (top, text="close window", command=top.destroy).pack()
+
+
+knapp = Button(root, text="Open second window", command=open).pack()
+
+
+#top = Toplevel()
+#label = Label(top, text="Hello World").pack()
+#my_img = ImageTk.PhotoImage(Image.open("bilde2.png"))
+#label1 = Label(top, image=my_img).pack
+
+
+
+mainloop()
